17f2b112b3d24cc95ce7691c9bfdcd587a28c375
ItsDandelia <69596233+ItsDandelia@users.noreply.github.com>
2020-10-17 09:52:57 +0530

    Update Whatsapp_Bot_Complete.py

--- Day 1/Whatsapp_Bot_Complete.py
+++ Day 1/Whatsapp_Bot_Complete.py
@@ -1,61 +1,38 @@
-#First download the chrome webdriver from https://chromedriver.chromium.org/downloads
-#Copy that .exe file in C:/Program Files(x86)
-from time import sleep
 from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException as se
+from time import sleep
+from selenium.common.exceptions import NoSuchElementException as nse
 
-
-#Search Function for non-recent chats
 def Search_chat(User):
-    #Clickng search bar
-    Search = driver.find_element_by_xpath('//div[@class="_2EoyP"]')
-    Search.click()
-    #Finding the User
-    Search_user = driver.find_element_by_xpath('//div[@class="_3FRCZ copyable-text selectable-text"]')
-    Search_user.clear()
-    Search_user.send_keys(User)
+    Search_bar = driver.find_element_by_xpath('//*[@id="side"]/div[1]')
+    Search_bar.click()
+    Search_User = driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]')
+    Search_User.clear()
+    Search_User.send_keys(User)
     sleep(1)
-    Search_Bar = driver.find_element_by_xpath('//span[@class="matched-text _3Whw5"]')
-    Search_Bar.click()
-    sleep(2)  # Increase if browser is slow
+    Search_list = driver.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[1]/div/div/div[2]/div[1]/div')
+    Search_list.click()
 
+QR_cookies = webdriver.ChromeOptions()
+QR_cookies.add_argument('--user-data-dir=C:\\Users\\amogh\\AppData\\Local\\Google\\Chrome\\User Data\\Default')
+QR_cookies.add_argument('--profile-data-directory=Default')
+QR_cookies.add_argument('start-maximized')
+QR_cookies.add_argument('--disable-infobars')
 
-#By using the QR_cache we only need to scan QR code once
-QR_cache = webdriver.ChromeOptions()
-# path C:\\Users\\<Your username>\\AppData\\Local\\Google\\Chrome\\User Data\\Default
-QR_cache.add_argument('--user-data-dir=C:\\Users\\amogh\\AppData\\Local\\Google\\Chrome\\User Data\\Default')
-QR_cache.add_argument('--profile-data-directory=Default')
-QR_cache.add_argument('start-maximized')
-QR_cache.add_argument('--disable-infobars')
+Path = "C:\\Program Files (x86)\\chromedriver.exe"
+driver = webdriver.Chrome(Path, options=QR_cookies)
+driver.get('Https://web.whatsapp.com')
+sleep(10)
 
-#Setup Chrome Web driver
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH,options=QR_cache)
-driver.get("https://web.whatsapp.com")
-sleep(12)
-
-#Fill the users in list you want to send message
-User_list= ['Abhijeet Jio','Onkar','Abhijeet Gaonkar','Raj','Jatin (SE CO)',
-            'Shubham (SE CO)','Omkar (SE CO)','Atharva Patil']
+User_list = ["Abhijeet Gaonkar","Abhijeet Jio", "Atharva Patil","Raj (TE)","Jatin (TE)","Omkar (SE CO)"]
 
 for User in User_list :
     try :
-        #Find user in recent chat
-        Search_Box = driver.find_element_by_xpath('//span[@title="{}"]'.format(User))
-        Search_Box.click()
-
-
-    except se :
+        Recent_chat = driver.find_element_by_xpath('//*[@title="{}"]'.format(User))
+        Recent_chat.click()
+    except nse:
         Search_chat(User)
 
-
-    #Find msg box windows and sends text
-    msg = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-    sleep(1)
-    msg.send_keys("Dream 11 pe team me bana leta hu aap ISA ka workshop attend karo")
-
-    #Find send button and click it
-    send_button = driver.find_element_by_xpath("//button[@class='_1U1xa']")
-    #send_button.click()
-    sleep(4)
+    msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+    msg_box.send_keys('Hi there, Whatsapp bot test')
+    send_button = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]')
+    send_button.click()
